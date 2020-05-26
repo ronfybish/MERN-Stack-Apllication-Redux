391fc2e472c26a9918a7aec338bfe2cae6037e9e
Ron Fybish <ron@theiavisionai.com>
2020-05-26 12:14:23 +0300

    initial dependencies

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json());
+
+// Define Routes
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
