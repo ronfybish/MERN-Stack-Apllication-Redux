551a48f5a17401d4f6210ab4df821e026084bdb3
Ron Fybish <ron@theiavisionai.com>
2020-05-31 09:34:38 +0300

    finish CRUD call for profile model

--- controller/profile.js
+++ controller/profile.js
@@ -26,57 +26,122 @@ module.exports = {
 	},
 
 	createOrUpdateProfile: async (req, res) => {
-        const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    const {
-      company,
-      location,
-      website,
-      bio,
-      skills,
-      status,
-      githubusername,
-      youtube,
-      twitter,
-      instagram,
-      linkedin,
-      facebook
-    } = req.body;
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+		const {
+			company,
+			location,
+			website,
+			bio,
+			skills,
+			status,
+			githubusername,
+			youtube,
+			twitter,
+			instagram,
+			linkedin,
+			facebook,
+		} = req.body;
 
-    const profileFields = {
-      user: req.user.id,
-      company,
-      location,
-      website: website && website !== '' ? normalize(website, { forceHttps: true }) : '',
-      bio,
-      skills: Array.isArray(skills)
-        ? skills
-        : skills.split(',').map((skill) => ' ' + skill.trim()),
-      status,
-      githubusername
-    };
+		const profileFields = {
+			user: req.user.id,
+			company,
+			location,
+			website:
+				website && website !== ''
+					? normalize(website, { forceHttps: true })
+					: '',
+			bio,
+			skills: Array.isArray(skills)
+				? skills
+				: skills.split(',').map(skill => ' ' + skill.trim()),
+			status,
+			githubusername,
+		};
 
-    // Build social object and add to profileFields
-    const socialfields = { youtube, twitter, instagram, linkedin, facebook };
+		// Build social object and add to profileFields
+		const socialfields = {
+			youtube,
+			twitter,
+			instagram,
+			linkedin,
+			facebook,
+		};
 
-    for (const [key, value] of Object.entries(socialfields)) {
-      if (value && value.length > 0)
-        socialfields[key] = normalize(value, { forceHttps: true });
-    }
-    profileFields.social = socialfields;
+		for (const [key, value] of Object.entries(socialfields)) {
+			if (value && value.length > 0)
+				socialfields[key] = normalize(value, { forceHttps: true });
+		}
+		profileFields.social = socialfields;
+
 		try {
 			let profile = await Profile.findOneAndUpdate(
-                { user: req.user.id },
-                { $set: profileFields },
-                { new: true, upsert: true }
-              );
-              res.json(profile);
+				{ user: req.user.id },
+				{ $set: profileFields },
+				{ new: true, upsert: true }
+			);
+			res.status(200).json(profile);
 		} catch (error) {
 			console.error(error.message);
 			res.status(500).json({
 				errors: [{ msg: 'Error Server **createOrUpdateProfile**' }],
+			});
+		}
+	},
+
+	deleteProfile: async (req, res) => {
+		try {
+			// Remove user posts
+
+			// Remove profile
+			await Profile.findOneAndRemove({ user: req.user.id });
+			// Remove user
+			await User.findOneAndRemove({ _id: req.user.id });
+
+			res.status(200).json({ msg: 'User deleted' });
+		} catch (error) {
+			console.error(error.message);
+			res.status(500).json({
+				errors: [{ msg: 'Error Server **deleteProfile**' }],
+			});
+		}
+	},
+
+	getAllProfiles: async (req, res) => {
+		try {
+			const profiles = await Profile.find().populate('user', [
+				'name',
+				'avatar',
+			]);
+			res.status(200).json(profiles);
+		} catch (err) {
+			console.error(error.message);
+			res.status(500).json({
+				errors: [{ msg: 'Error Server **getAllProfile**' }],
+			});
+		}
+	},
+
+	getProfileByUserId: async (req, res) => {
+		const user_id = req.params.user_id;
+		try {
+			const profile = await Profile.findOne({
+				user: user_id,
+			}).populate('user', ['name', 'avatar']);
+
+			if (!profile)
+				return res.status(400).json({ msg: 'Profile not found' });
+
+			return res.status(200).json(profile);
+		} catch (error) {
+			console.error(error.message);
+			if (error.kind == 'ObjectId') {
+				return res.status(400).json({ msg: 'Profile not found' });
+			}
+			res.status(500).json({
+				errors: [{ msg: 'Error Server **getProfileByUserId**' }],
 			});
 		}
 	},
--- routes/profile.js
+++ routes/profile.js
@@ -4,15 +4,21 @@ const { check } = require('express-validator');
 
 const profileValidator = [
 	check('status', 'Status is required').not().isEmpty(),
-	check('skils', 'Skils is required').not().isEmpty(),
+	check('skills', 'Skills is required').not().isEmpty(),
 ];
 
 const {
     getProfile,
-    createOrUpdateProfile
+    createOrUpdateProfile,
+    getAllProfiles,
+    getProfileByUserId,
+    deleteProfile,
 } = require('../controller/profile');
 
 router.get('/me',auth,getProfile);
+router.get('/',getAllProfiles);
+router.delete('/',auth,deleteProfile);
+router.get('/user/:user_id',getProfileByUserId);
 router.post('/',[auth,profileValidator],createOrUpdateProfile);
 
 module.exports = router;
