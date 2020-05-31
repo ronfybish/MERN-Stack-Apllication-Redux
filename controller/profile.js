f973aa86345344691616f02827ac98dca378d63b
Ron Fybish <ron@theiavisionai.com>
2020-05-31 11:02:36 +0300

    complete profile section server side

--- controller/profile.js
+++ controller/profile.js
@@ -1,6 +1,9 @@
 const Profile = require('../models/Profile');
 const User = require('../models/User');
 const normalize = require('normalize-url');
+const GITHUB_CLIENT_ID = require('config').get('GITHUB_CLIENT_ID');
+const GITHUB_CLIENT_SECRET = require('config').get('GITHUB_CLIENT_SECRET');
+const axios = require('axios');
 const { validationResult } = require('express-validator');
 
 module.exports = {
@@ -143,6 +146,137 @@ module.exports = {
 			res.status(500).json({
 				errors: [{ msg: 'Error Server **getProfileByUserId**' }],
 			});
+		}
+	},
+
+	addProfileExperience: async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const {
+			title,
+			company,
+			location,
+			from,
+			to,
+			current,
+			description,
+		} = req.body;
+
+		const newExp = {
+			title,
+			company,
+			location,
+			from,
+			to,
+			current,
+			description,
+		};
+
+		try {
+			const profile = await Profile.findOne({ user: req.user.id });
+
+			profile.experience.unshift(newExp);
+
+			await profile.save();
+
+			res.status(200).json(profile);
+		} catch (error) {
+			console.error(error.message);
+			return res.status(500).json({
+				errors: [{ msg: 'Error Server **addProfileExperience**' }],
+			});
+		}
+	},
+
+	deleteProfileExperience: async (req, res) => {
+		try {
+			const foundProfile = await Profile.findOne({ user: req.user.id });
+
+			foundProfile.experience = foundProfile.experience.filter(
+				exp => exp._id.toString() !== req.params.exp_id
+			);
+
+			await foundProfile.save();
+			return res.status(200).json(foundProfile);
+		} catch (error) {
+			console.error(error.message);
+			return res.status(500).json({
+				errors: [{ msg: 'Error Server **deleteProfileExperience**' }],
+			});
+		}
+	},
+
+	addProfileEducation: async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const {
+			school,
+			degree,
+			fieldofstudy,
+			from,
+			to,
+			current,
+			description,
+		} = req.body;
+
+		const newEdu = {
+			school,
+			degree,
+			fieldofstudy,
+			from,
+			to,
+			current,
+			description,
+		};
+
+		try {
+			const profile = await Profile.findOne({ user: req.user.id });
+
+			profile.education.unshift(newEdu);
+
+			await profile.save();
+
+			res.json(profile);
+		} catch (error) {
+			console.error(error.message);
+			res.status(500).json({
+				errors: [{ msg: 'Error Server **addProfileEducation**' }],
+			});
+		}
+	},
+
+	deleteProfileEducation: async (req, res) => {
+		try {
+			const foundProfile = await Profile.findOne({ user: req.user.id });
+			foundProfile.education = foundProfile.education.filter(
+				edu => edu._id.toString() !== req.params.edu_id
+			);
+			await foundProfile.save();
+			return res.status(200).json(foundProfile);
+		} catch (error) {
+			console.error(error);
+			return res.status(500).json({
+				errors: [{ msg: 'Error Server **deleteProfileEducation**' }],
+			});
+		}
+	},
+
+	getUserRepos: async (req, res) => {
+		try {
+			const gitHubResponse = await axios.get(
+				`https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${GITHUB_CLIENT_ID}&client_secret=${GITHUB_CLIENT_SECRET}`
+			);
+
+			return res.status(200).json(gitHubResponse.data);
+		} catch (err) {
+			console.error(err.message);
+			return res.status(404).json({ msg: 'No Github profile found' });
 		}
 	},
 };
