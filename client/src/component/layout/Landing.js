e26e51c6c75f48e4176064af37f218bfe21f89d9
Ron <fybishron@gmail.com>
2020-06-23 14:58:31 +0300

    delete account client side

--- client/src/component/layout/Landing.js
+++ client/src/component/layout/Landing.js
@@ -9,10 +9,9 @@ const Landing = ({isAuthenticated}) => {
 		<section className='landing'>
 			<div className='dark-overlay'>
 				<div className='landing-inner'>
-					<h1 className='x-large'>Developer Connector</h1>
+					<h1 className='x-large'>Developer Info</h1>
 					<p className='lead'>
-						Create a developer profile/portfolio, share posts and
-						get help from other developers
+						Create a developer live resume
 					</p>
 					<div className='buttons'>
 						<Link to='/register' className='btn btn-primary'>
