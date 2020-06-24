28b1f3d77ed6a3ec647e72d36959accdbde57630
Ron <fybishron@gmail.com>
2020-06-24 14:40:22 +0300

    prepare for deploy

---
+++ client/build/precache-manifest.61adf8ca5dbfdde0c02274600a78fa81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d54b9e63fc91f68c583ed280a2bb30d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04b0fdfd2c40f10f378b",
+    "url": "/static/css/main.20553421.chunk.css"
+  },
+  {
+    "revision": "b1e840f513d3bb76395b",
+    "url": "/static/js/2.e275791a.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.e275791a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04b0fdfd2c40f10f378b",
+    "url": "/static/js/main.bc9f02ec.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
